6f7b16d771e43653adddea474a5f66e3ad32c36a
Artem Zubkov <17artem12@gmail.com>
2026-02-22 22:16:45 +0500

    ParallelClusterClient.cs done

--- homework 2/ClusterClient/Clients/ParallelClusterClient.cs
+++ homework 2/ClusterClient/Clients/ParallelClusterClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,38 @@ namespace ClusterClient.Clients
         {
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            throw new NotImplementedException();
+            var timeoutTask = Task.Delay(timeout).ContinueWith<string>(_ => throw new TimeoutException());
+            
+            var uris = ReplicaAddresses.Select(async uri =>
+            {
+                var webRequest = CreateRequest(uri + "?query=" + query);
+                Log.InfoFormat($"Processing {webRequest.RequestUri}");
+                var resultTask = await ProcessRequestAsync(webRequest);
+                return resultTask;
+
+            }).ToList();
+            uris.Add(timeoutTask);
+
+            while (uris.Any())
+            {
+                try
+                {
+                    var resultTask = await Task.WhenAny(uris);
+                    if (resultTask == timeoutTask)
+                        await timeoutTask; 
+                    return await resultTask;
+                }
+                catch (Exception e)
+                {
+                    if (uris.Count == 2) 
+                        throw;
+                    else
+                        uris.Remove(uris.First());
+                }
+            }
+             throw new TimeoutException();
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(ParallelClusterClient));
