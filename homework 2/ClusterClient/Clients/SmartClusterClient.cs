f7140db95821087a1a2dba07d8aec63c84355eca
Artem Zubkov <17artem12@gmail.com>
2026-02-25 17:17:31 +0500

    SmartClusterClient.cs done

--- homework 2/ClusterClient/Clients/SmartClusterClient.cs
+++ homework 2/ClusterClient/Clients/SmartClusterClient.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using log4net;
 
@@ -13,9 +15,47 @@ namespace ClusterClient.Clients
         {
         }
 
-        public override Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
+        public override async Task<string> ProcessRequestAsync(string query, TimeSpan timeout)
         {
-            throw new NotImplementedException();
+            {
+                var partOfTimeout = timeout / ReplicaAddresses.Length;
+                var tasksAtWork =  new List<Task<string>>();
+                var timeoutGlobal = Task.Delay(timeout).ContinueWith<string>(_ => throw new TimeoutException());
+                tasksAtWork.Add(timeoutGlobal);
+                
+                foreach (var replicaAddress in ReplicaAddresses)
+                {
+                    var webRequest = CreateRequest(replicaAddress + "?query=" + query);
+                    Log.InfoFormat($"Processing {webRequest.RequestUri}");
+                    var replicaTask = ProcessRequestAsync(webRequest);
+                    var timeoutDelay = Task.Delay(partOfTimeout).ContinueWith<string>(_ => throw new TimeoutException());
+                    
+                    tasksAtWork.Add(timeoutDelay);
+                    tasksAtWork.Add(replicaTask);
+                    
+                    var taskInCycle = await Task.WhenAny(tasksAtWork);
+                    tasksAtWork.Remove(timeoutDelay);
+
+                    if (taskInCycle == timeoutGlobal)
+                        throw new  TimeoutException();
+                    
+                    if (taskInCycle == timeoutDelay)
+                        continue;
+
+                    try
+                    {
+                        return await taskInCycle;
+                    }
+                    catch (Exception e)
+                    {
+                        tasksAtWork.Remove(taskInCycle);
+                        continue;
+                    }
+                        
+                }
+                var resultTask = await Task.WhenAny(tasksAtWork);
+                return await resultTask;
+            }
         }
 
         protected override ILog Log => LogManager.GetLogger(typeof(SmartClusterClient));
